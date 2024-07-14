154ec86388dd64b176d542859944dd6e8aa9f4df
Tejas <tejaskandri28@gmail.com>
2024-07-14 17:11:35 -0400

    Car basic movements

---
+++ main.js
@@ -0,0 +1,21 @@
+//select canvas and set the width and height
+//the canvas is the road in the middle
+const canvas = document.querySelector('#myCanvas');
+canvas.width = 200;
+
+
+//car
+const ctx = canvas.getContext('2d');
+const car = new Car(100,100,30,50);
+car.draw(ctx);
+
+animate();
+
+
+function animate()
+{
+    canvas.height = window.innerHeight;
+    car.update();
+    car.draw(ctx);
+    requestAnimationFrame(animate);
+}
