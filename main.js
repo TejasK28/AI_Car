d9bd9f12478815cacce7144b3ba3fb4ee360364c
Tejas <tejaskandri28@gmail.com>
2024-07-23 15:37:13 -0400

    Sensor readings

--- main.js
+++ main.js
@@ -1,23 +1,16 @@
-//select canvas and set the width and height
-//the canvas is the road in the middle
-const canvas = document.querySelector('#myCanvas');
-canvas.width = 200;
+const canvas=document.getElementById("myCanvas");
+canvas.width=200;
 
-
-//car
-const ctx = canvas.getContext('2d');
-const road = new Road(canvas.width/2, canvas.width * 0.9);
-const car = new Car(road.gatLaneCenter(1),100,30,50);
-car.draw(ctx);
+const ctx = canvas.getContext("2d");
+const road=new Road(canvas.width/2,canvas.width*0.9);
+const car=new Car(road.getLaneCenter(1),100,30,50);
 
 animate();
 
+function animate(){
+    car.update(road.borders);
 
-function animate()
-{
-    
-    car.update();
-    canvas.height = window.innerHeight;
+    canvas.height=window.innerHeight;
 
     ctx.save();
     ctx.translate(0,-car.y+canvas.height*0.7);
@@ -26,6 +19,5 @@ function animate()
     car.draw(ctx);
 
     ctx.restore();
-    
     requestAnimationFrame(animate);
 }
