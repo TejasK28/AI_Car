aefdb6981dd7e17d4297b575c2b673039da11942
Tejas <tejaskandri28@gmail.com>
2024-07-23 20:55:15 -0400

    sensor spread update

--- sensor.js
+++ sensor.js
@@ -3,7 +3,7 @@ class Sensor{
         this.car=car;
         this.rayCount=5;
         this.rayLength=150;
-        this.raySpread=Math.PI/2;
+        this.raySpread=Math.PI/1.5;
 
         this.rays=[];
         this.readings=[];
